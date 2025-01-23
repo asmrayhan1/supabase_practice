4abef26a186a39f7797e2ef7edc95928fbb98adb
asmrayhan1 <chowdhuryr828@gmail.com>
2025-01-23 18:54:13 +0600

    not fix

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,14 +10,40 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late final SupabaseClient _supabaseClient;
+  List<String> imageUrls = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _supabaseClient = Supabase.instance.client;
+    _fetchImages();
+  }
+
+  // Fetch all image URLs
+  Future<void> _fetchImages() async {
+    final response = await _supabaseClient.storage.from('your-bucket-name').list('images/');
+
+    if (response.error != null) {
+      print('Error fetching images: ${response.error?.message}');
+    } else {
+      setState(() {
+        imageUrls = response.data!.map<String>((e) => _supabaseClient.storage.from('your-bucket-name').getPublicUrl(e.name).toString()).toList();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-
-        ],
+      appBar: AppBar(title: Text('Image List')),
+      body: imageUrls.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+        itemCount: imageUrls.length,
+        itemBuilder: (context, index) {
+          return Image.network(imageUrls[index]);
+        },
       ),
     );
   }
